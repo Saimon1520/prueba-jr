6b6da7dc16dcaa312d39d50491236a8d7b5e59ea
Saimon1520 <111163588+Saimon1520@users.noreply.github.com>
2024-12-24 04:04:46 -0600

    Add the PostForm used to add or edit a publication.

---
+++ src/components/PostForm.tsx
@@ -0,0 +1,66 @@
+'use client';
+
+import { useState, useEffect } from 'react';
+import { usePostContext } from '../context/PostContext';
+
+interface PostFormProps {
+    postId?: number;  // Si se pasa el id, es para editar el post
+}
+
+const PostForm = ({ postId }: PostFormProps) => {
+    const { addPost, editPost, posts } = usePostContext();
+    const [title, setTitle] = useState('');
+    const [body, setBody] = useState('');
+    const [message, setMessage] = useState('');
+
+    // Si hay un postId, busca el post correspondiente para editarlo
+    useEffect(() => {
+        if (postId) {
+            const postToEdit = posts.find(post => post.id === postId);
+            if (postToEdit) {
+                setTitle(postToEdit.title);
+                setBody(postToEdit.body);
+            }
+        }
+    }, [postId, posts]);
+
+    const handleSubmit = (e: React.FormEvent) => {
+        e.preventDefault();
+        if (postId) {
+            editPost(postId, title, body);
+            setMessage('La publicación ha sido editada!');
+        } else {
+            addPost(title, body);
+            setMessage('La publicación ha sido agregada!');
+        }
+        setTitle('');
+        setBody('');
+    };
+
+    return (
+        <form onSubmit={handleSubmit} className="mb-8">
+            {message? <p className="text-green-500 text-center">{message}</p> : ""}
+            <input
+                type="text"
+                placeholder="Title"
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+                className="border p-2 mb-4 w-full text-black"
+                required
+            />
+            <textarea
+                placeholder="Body"
+                value={body}
+                onChange={(e) => setBody(e.target.value)}
+                className="border p-2 w-full text-black"
+                required
+            ></textarea>
+            <button type="submit" className="bg-purple-700 text-white p-2 mt-4">
+                {postId ? 'Edit Post' : 'Add Post'}
+            </button>
+        </form>
+    );
+};
+
+export default PostForm;
+
