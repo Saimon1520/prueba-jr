f2fe6a7d153a411185d029fea8adf7418bd28c7c
Saimon1520 <111163588+Saimon1520@users.noreply.github.com>
2024-12-29 11:34:47 -0600

    add a user context

---
+++ src/context/UserContext.tsx
@@ -0,0 +1,78 @@
+'use client';
+
+import React, { createContext, useState, useContext, ReactNode } from 'react';
+
+// Definir los tipos de datos para el usuario
+interface Geo {
+  lat: string;
+  lng: string;
+}
+
+interface Address {
+  street: string;
+  suite: string;
+  city: string;
+  zipcode: string;
+  geo: Geo;
+}
+
+interface Company {
+  name: string;
+  catchPhrase: string;
+  bs: string;
+}
+
+interface User {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  password?: string;
+  phone?: string;
+  website?: string;
+  address: Address;
+  company: Company;
+}
+
+interface UserContextType {
+  user: User | null;
+  addUser: (userData: User) => void;
+  updateUser: (updatedUser: User) => void;
+  error: string | null;
+  setError: (error: string) => void;
+}
+
+const UserContext = createContext<UserContextType | undefined>(undefined);
+
+export const useUserContext = () => {
+  const context = useContext(UserContext);
+  if (!context) {
+    throw new Error('useUserContext must be used within a UserProvider');
+  }
+  return context;
+};
+
+interface UserProviderProps {
+  children: ReactNode;
+}
+
+export const UserProvider = ({ children }: UserProviderProps) => {
+  const [user, setUser] = useState<User | null>(null);
+  const [error, setError] = useState<string | null>(null);
+
+  // Función para agregar un nuevo usuario
+  const addUser = (userData: User) => {
+    setUser(userData);
+  };
+
+  // Función para actualizar la información del usuario
+  const updateUser = (updatedUser: User) => {
+    setUser(updatedUser);
+  };
+
+  return (
+    <UserContext.Provider value={{ user, addUser, updateUser, error, setError }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
